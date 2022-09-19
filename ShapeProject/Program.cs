653080ba82c19a66265f70f004588bff5910231b
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-19 08:29:04 +0530

    Add project files.

---
+++ ShapeProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c = new Circle(1);
+            c.Area();
+            Console.WriteLine(c);
+
+        }
+    }
+}
---
+++ ShapeProject/Shape.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeProject
+{
+    abstract public class Shape
+    {
+        public abstract void Area();
+    }
+    public class Circle : Shape
+    {
+        private double radius;
+        public double result;
+        public Circle(int radius)
+        {
+            this.radius = radius;
+            
+        }
+        public override void Area()
+        {
+            result = 3.14 * radius * radius;
+        }
+        public override string ToString()
+        {
+            return $"Area of circle is {result}";
+        }
+
+    }
+}
